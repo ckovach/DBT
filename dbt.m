3f05fcbb62136784acd05ca4771500596f010497
ckovach <ckovach@4e33aaf1-3bdf-43e8-baf7-e7e736085cc8>
2014-02-11 18:38:57 +0000

    Correctly report the default shoulder, as this has changed.
    git-svn-id: https://saccade.neurosurgery.uiowa.edu/svn/KovachToolbox@458 4e33aaf1-3bdf-43e8-baf7-e7e736085cc8

--- dbt.m
+++ dbt.m
@@ -18,7 +18,8 @@ classdef dbt
 %  	offset   -  offset of the first band from 0hz (default = 0)
 %       padding  - 'time': pad signal in the time domain, changing duration (default)
 %                  'frequency': pad in the frequency domain, changing sampling rate
-%       shoulder - (0 - 1) degree of overlap between neighboring bands (default = 0)           
+%       shoulder - (0 - 1) degree of overlap between neighboring bands (default = 1). 
+%                  Note that, at present, 1 is the maximum allowable value.
 %
 %  The overlapping portions of the bands are windowed with a  taper:
 %	     ____   __________   ________   _________
