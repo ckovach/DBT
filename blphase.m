37b2bc8130f04f7679f2d5003cf3699f2aa77dd0
ckovach <ckovach@4e33aaf1-3bdf-43e8-baf7-e7e736085cc8>
2016-03-03 03:04:38 +0000

    First version of the band-limited phase algorithm
    git-svn-id: https://saccade.neurosurgery.uiowa.edu/svn/KovachToolbox@716 4e33aaf1-3bdf-43e8-baf7-e7e736085cc8

---
+++ blphase.m
@@ -0,0 +1,51 @@
+function [dbout,PH,dbx]=blphase(x,fs,bw,varargin)
+
+%
+% [dbout,PH,dbx] = blphase(x,fs,bw)
+%
+% Band limited phase using iterative filtering 
+% After each normalization the Hilbert transform is reapplied.
+%
+% dbout is a dbt object containing the normalized signal. H contains
+% the reconstructed and amplitude normalized signal for each band at the
+% original sampling rate.
+%%
+tol=1e-5;
+% DBT with hilbert equivalent trsfm
+dbx = dbt(x,fs,bw,'centerdc',false,'shoulder',0,varargin{:});
+
+S = dbx.blrep./abs(dbx.blrep);
+
+niter=0;
+del=Inf;
+dels=[];
+while del > tol
+    
+     niter=niter+1;
+    del
+    
+    F = fft(S);
+    
+    F(end/(2+2*dbx.fftpad)+1:end,:,:)=0;
+    
+    Snew=ifft(F);
+    
+    Snew=Snew./abs(Snew);
+    del = sum(abs(Snew(:)-S(:)).^2)./sum(abs(S(:)).^2);
+    
+    S = Snew;
+    
+   % dels(end+1)=del;
+end
+
+dbout=dbx;
+dbout.blrep=S;
+
+
+if nargout > 1
+    PH = zeros(length(x),length(dbout.frequency));
+    for k = 1:length(dbout.frequency), 
+        PH(:,k)=dbout.signal(k,1);
+    end    
+end
+PH=PH./abs(PH);
