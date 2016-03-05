2ef4e60254bddc4f5fa14a2209007f39b1b179e4
ckovach <ckovach@4e33aaf1-3bdf-43e8-baf7-e7e736085cc8>
2016-03-05 05:34:44 +0000

    Make window even to avoid inconsistencies related to rounding from within tolerance of.5
    git-svn-id: https://saccade.neurosurgery.uiowa.edu/svn/KovachToolbox@721 4e33aaf1-3bdf-43e8-baf7-e7e736085cc8

--- dbt.m
+++ dbt.m
@@ -230,7 +230,7 @@ classdef dbt
            newn = ceil(n/den)*den;
            newT = newn/fs;
            
-           winN = round(bw*newT);
+           winN = round(bw*newT/2)*2;
            newbw = winN/newT;
            
 
