11a84708dded3dcc520114130e4b64ac990a2e96
ckovach <ckovach@4e33aaf1-3bdf-43e8-baf7-e7e736085cc8>
2016-11-01 17:42:17 +0000

    Reorganized output and input options into structs
    git-svn-id: https://saccade.neurosurgery.uiowa.edu/svn/KovachToolbox@771 4e33aaf1-3bdf-43e8-baf7-e7e736085cc8

--- dbtbicoh.m
+++ dbtbicoh.m
@@ -1,4 +1,4 @@
-function [BICOH,bspect,w1,w2,NRM,dbx1,dbx2,A] = dbtbicoh(x,fs,bw,varargin)
+function [out,dbx,tdcomp] = dbtbicoh(x,fs,bw,varargin)
 
 % Computes bicoherence with DBT.
 %
@@ -16,45 +16,53 @@ function [BICOH,bspect,w1,w2,NRM,dbx1,dbx2,A] = dbtbicoh(x,fs,bw,varargin)
 
 warning('This script is under development and is not stable. Do not use it unless you know what you''re doing')
 
-%if nargin < 3
-    bw2x = 4;
-%end
-downsamp2 = true; 
-symmetric = false;
-upsampfx = 0;
-rotation = 'ww';
-maxfreq = Inf;
-type = 'bbb';
-do_svd = false;
-  i = 1;   
-  
+opts = struct(...
+'bw2x',4,...
+'symmetric',false,...
+'upsampfx',0,...
+'rotation','ww',...
+'maxfreq',Inf,...
+'type','bbb',...
+'do_svd',false);
+
+if nargin < 4 && isstruct(varargin{1})
+    newopts = varargin{1};
+    fn = fieldnames(newopts);
+    for k = 1:lengt(fn)
+        opts.(fn{k}) = newopts.(fn{k});
+    end
+    varargin(1) = [];
+end
+    
+
+i = 1;     
 while i < length(varargin)
   switch lower(varargin{i})
 
       case {'type'}
-          type = varargin{i+1};
-          if strcmpi(type,'bbb')||strcmpi(type,'nnn')
-              bw2x = 1;
+          opts.type = varargin{i+1};
+          if strcmpi(opts.type,'bbb')||strcmpi(opts.type,'nnn')
+              opts.bw2x = 1;
           end
           i = i+1;
       case {'symmetric'}
-          symmetric = varargin{i+1};
+          opts.symmetric = varargin{i+1};
           i = i+1;
       case {'maxfreq'}
-          maxfreq = varargin{i+1};
+          opts.maxfreq = varargin{i+1};
           i = i+1;
       case {'upsampfx'}
-          upsampfx = varargin{i+1};
+          opts.upsampfx = varargin{i+1};
           i = i+1;
       case {'bw2x'} % bw2 relative to bw when bandwidths differ
-         bw2x = varargin{i+1};
+         opts.bw2x = varargin{i+1};
           i = i+1;
       case {'bw2'} % bw2 relative to bw when bandwidths differ
-         bw2x = varargin{i+1}/bw;
+         opts.bw2x = varargin{i+1}/bw;
           i = i+1;
              
         case {'svd'} % bw2 relative to bw when bandwidths differ
-         do_svd = varargin{i+1};
+         opts.do_svd = varargin{i+1};
           i = i+1;
            
       otherwise
@@ -63,28 +71,28 @@ while i < length(varargin)
   i=i+1;
 end
 
-bw2 = bw2x*bw;      
-lpf = min(fs/2,maxfreq*2);
+bw2 = opts.bw2x*bw;      
+lpf = min(fs/2,opts.maxfreq*2);
 nch = size(x,2);
-switch lower(type)
+switch lower(opts.type)
     case {'single','bbb','nnn','nnb','nbb','bnb'} 
         %% The standard approach
                 
                 
             
-                 switch lower(type)
+                 switch lower(opts.type)
                      case {'nnb','nbb','bnb'}
                          upsamptx = bw2/bw-1;
                        %  upsamptx = 0;
-                         upsampf2 = (upsampfx+1)*(upsamptx+1)-1;
+                         upsampf2 = (opts.upsampfx+1)*(upsamptx+1)-1;
                           dbx2 = dbt(x,fs,bw2,'remodphase',false,'upsampleFx',upsampf2,'lowpass',lpf);
-                     %     dbx2 = dbt(x,fs,bw2,'remodphase',false,'upsampleFx',upsampfx);
-                          dbx1 = dbt(x,fs,bw,'upsampleFx',upsampfx,'remodphase',false,'upsampleTx',upsamptx,'lowpass',lpf);
+                     %     dbx2 = dbt(x,fs,bw2,'remodphase',false,'upsampleFx',opts.upsampfx);
+                          dbx1 = dbt(x,fs,bw,'upsampleFx',opts.upsampfx,'remodphase',false,'upsampleTx',upsamptx,'lowpass',lpf);
                     
                      otherwise
                          
                          upsamptx = 1;
-                          dbx1 = dbt(x,fs,bw,'upsampleFx',upsampfx,'remodphase',false,'upsampleTx',upsamptx-1,'lowpass',lpf);
+                          dbx1 = dbt(x,fs,bw,'upsampleFx',opts.upsampfx,'remodphase',false,'upsampleTx',upsamptx-1,'lowpass',lpf);
                           dbx2 = dbx1;
                  end
                  tol = dbx1.fullFS./dbx1.fullN/2;
@@ -95,33 +103,33 @@ switch lower(type)
                  keept1 = dbx1.time <=dbx2.time(end) ;
                  keept2 = dbx2.time <=dbx1.time(end);%& abs(mod(dbx2.time,1./dbx1.sampling_rate))<tol/dbx1.sampling_rate;
                  keept1(sum(keept2)+1:end)=[];
-                 getfreq = min(maxfreq,dbx1.fullFS/2);
+                 getfreq = min(opts.maxfreq,dbx1.fullFS/2);
                 
                  getf = dbx1.frequency<=getfreq & dbx1.frequency>=-1e-9; %& abs(mod(dbx1.frequency,fstep2))<tol;
                  getf2 = dbx2.frequency<=getfreq & dbx2.frequency>=-1e-9;
                  w1 = dbx1.frequency(getf);
                  w2 = dbx2.frequency(getf2);
-                 if ~symmetric
+                 if ~opts.symmetric
                       w1keep = (1:sum(getf)/2);
                       w1 = w1(w1keep);
 
                  else
                        w1keep = (1:sum(getf));
                  end
-                 switch rotation
+                 switch opts.rotation
                      case 'ww'
                          
                          %[W1,W2] = ndgrid(1:sum(getf),1:sum(getf2));   
                          if nch > 1
-                            [W1,W2,WCh1,WCh2] = ndgrid(w1keep,1:bw2x:sum(getf2),1:nch,1:nch);      
+                            [W1,W2,WCh1,WCh2] = ndgrid(w1keep,1:opts.bw2x:sum(getf2),1:nch,1:nch);      
                          else
-                             [W1,W2] = ndgrid(w1keep,1:bw2x:sum(getf2));
+                             [W1,W2] = ndgrid(w1keep,1:opts.bw2x:sum(getf2));
                              WCh1 = 1;
                              WCh2 = 1;
                          end
                          W3 = round((w1(W1)+w2(W2)-w2(1))./fstep3)+1;
                          
-                         w2 = w2(1:bw2x:sum(getf2));
+                         w2 = w2(1:opts.bw2x:sum(getf2));
                      case 'vv'
                          [V1,V2] = ndgrid(1:sum(getf),1:sum(getf2));        
                         W1=(V1-V2)+1;
@@ -132,7 +140,7 @@ switch lower(type)
              
                  %  inds = W3<=length(dbx.frequency);
                  inds = W1>0 & W2 <=sum(getf2)& W3<=sum(getf2);
-                 if ~symmetric
+                 if ~opts.symmetric
                     inds = inds & W1 <=W2; 
                  end
                  blrep = dbx1.blrep(keept1,getf,:);
@@ -146,9 +154,9 @@ switch lower(type)
                  I3=W3 + Ich2;
                       
                 % I1=V1;I2=V2;I3=V3;
-%                   if strcmpi(type,'nbb') ||strcmpi(type,'bbb') || strcmpi(type,'nnn')
+%                   if strcmpi(opts.type,'nbb') ||strcmpi(opts.type,'bbb') || strcmpi(opts.type,'nnn')
                   
-                 if strcmpi(type,'bnb')
+                 if strcmpi(opts.type,'bnb')
                       W1x = W2;
                       W2 = W1;
                       W1 = W1x;
@@ -165,10 +173,10 @@ switch lower(type)
                      blrep2 = reshape(blrep2,size(blrep2,1),size(blrep2,2)*nch);
                      cblrep = reshape(cblrep,size(cblrep,1),size(cblrep,2)*nch);
                  end
-%                   elseif  strcmpi(type,'bnb')
+%                   elseif  strcmpi(opts.type,'bnb')
 %                           cblrep = conj(blrep);
 %                           blrep2 = dbx2.blrep(keept2,getf2,:);
-%                   elseif  strcmpi(type,'bbb') || strcmpi(type,'nnn')
+%                   elseif  strcmpi(opts.type,'bbb') || strcmpi(opts.type,'nnn')
 %                           cblrep = conj(blrep);
 %                           blrep2 = blrep;
 %                   end
@@ -180,25 +188,35 @@ switch lower(type)
 %                 NRM(inds) = sqrt(sum(abs(blrep(:,I1(inds),:)).^2).* sum(abs(cblrep(:,I3(inds),:).*blrep2(:,I2(inds),:)).^2));
              
 end
-   BICOH = bspect./NRM;
-                     
-   
-   BICOH(isnan(BICOH))=0;
-   
- if do_svd 
-    [u,l,v] = svd(BICOH); 
+out.BICOH = bspect./NRM;
+out.bspect = bspect;
+out.NRM = NRM;
+out.w1 = w1;
+out.w2 = w2;
+
+out.BICOH(isnan(out.BICOH))=0;
+dbx = [dbx1,dbx2];
+
+if opts.do_svd 
+    [u,l,v] = svd(out.BICOH); 
     getn = sum(diag(l)>0);
-    [uind,vind] = ndgrid(1:size(BICOH,1),1:size(BICOH,2));
+    [uind,vind] = ndgrid(1:size(out.BICOH,1),1:size(out.BICOH,2));
     U = u(uind(inds),1:getn);
     V = v(vind(inds),1:getn);
-    switch type
+    switch opts.type
         case 'nbb'
-                
+
             A =  (blrep(:,I1(inds),:).*blrep2(:,I2(inds),:).* cblrep(:,I3(inds),:))*(repmat(NRM(inds).^-1,1,getn).*(V.*conj(U))*diag(diag(l).^-.5));
         otherwise
-            warning('SVD not yet implemented for case %s',type)
+            warning('SVD not yet implemented for case %s',opts.type)
             A = nan;
- end
- else
+    end
+    tdcomp.Act = A;
+    tdcomp.time = dbx1.time;
+    tdcomp.u = u;
+    tdcomp.l = diag(l);
+    tdcomp.v = diag(v);
+
+else
      A = nan;
- end
+end
